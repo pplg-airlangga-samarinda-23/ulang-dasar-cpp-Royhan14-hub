a61fbc8733d7afa7954ab952869c485a3cbc7b83
Royhan14-hub <ayman.shanas2008@gmail.com>
2024-07-31 11:32:36 +0800

    selesai tugas

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,55 @@
 #include <iostream>
 using namespace std;
 
+#include <iostream>
+using namespace std;
+
+void cetakdata(string nama, string nisn, string jurusan, string tinggibadan) {
+    cout << "Nama: " << nama << endl;
+    cout << "NISN: " << nisn << endl;
+    cout << "Jurusan: " << jurusan << endl;
+    cout << "Tinggi Badan: " << tinggibadan << endl;
+}
+
 int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
+    string nama = "Ananda Gusti Sultan";
+    string nisn = "";
+    string jurusan = "PPLG";
+    string tinggibadan = "170";
+    
+    cetakdata(nama, nisn, jurusan, tinggibadan);
+    
     return 0;
 }
+
+// void cetakData(string nama, string nisn, string jurusan, string tinggibadan)
+// {
+//     // cetak data-data nama, nisn, jurusan dan tinggi badan
+// }
+
+// int main() {
+
+    // string nama, nisn, jurusan, tinggibadan;
+    // void cetakData(string nama, string nisn, string jurusan, string tinggibadan);
+    // cout << "Nama : " << endl;
+    // cin >> nama;
+
+    // cout << "Nisn : " << endl;
+    // cin >> nisn;
+
+    // cout << "Jurusan : " << endl;
+    // cin >> jurusan;
+
+    // cout << "TinggiBadan : " << endl;
+    // cin >> tinggibadan;
+
+    //buat variabel untuk menyimpan
+    // nama
+    // nisn
+    // jurusan
+    // tinggi badan
+
+    // panggil function cetakData
+
+    // return 0; 
+// }
